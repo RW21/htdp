45b63b1aec2c7c40dab594c91103e6e5300983ff
RW21 <rinze.watanabe.translation@gmail.com>
2020-07-07 13:03:23 +1000

    Update

--- htdp1.rkt
+++ htdp1.rkt
@@ -1,2 +1,16 @@
 #lang racket
 
+; ex 11-20
+(define (distance_of_point x y) (sqrt (+ (sqr x) (sqr y))))
+(define (cvolume x) (* x x x))
+(define (string-first s) (substring s 0 1))
+(define (string-last s) (string-ref s (sub1 (string-length s))))
+(define (==> sunny friday) (or (not sunny) friday))
+(define (string-join a b) (string-append a "_" b))
+(define (string-insert str i) (string-append (substring str 0 i)
+                                             "_"
+                                             (substring str i (string-length str))))
+(define (string-delete str i) (string-append (substring str 0 i)
+                                             (substring str (add1 i) (string-length str))))
+                                                                     
+                                  
